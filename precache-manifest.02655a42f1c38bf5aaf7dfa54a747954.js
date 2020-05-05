b23a4346a0f37e5fd23f875dfb9e90b63e7a2d2b
Jehiel Martinez <jehielmartinez@gmail.com>
2020-05-04 23:06:46 -0600

    Updates

---
+++ precache-manifest.02655a42f1c38bf5aaf7dfa54a747954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c57a5937418515d77e9971b37f2f74",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "88f86608161f2419c833",
+    "url": "/tictactoe/static/css/main.05084f0a.chunk.css"
+  },
+  {
+    "revision": "e61068f11173fe162e18",
+    "url": "/tictactoe/static/js/2.8368fdb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.8368fdb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f86608161f2419c833",
+    "url": "/tictactoe/static/js/main.eb4c8793.chunk.js"
+  },
+  {
+    "revision": "8cd3739712635f2cd5b8",
+    "url": "/tictactoe/static/js/runtime-main.148deea7.js"
+  }
+]);
